53cfbba63bf3bbc7e9247da5e7c19fc389163770
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-02-02 21:06:48 +0700

    add bai5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,4 @@
 <h1 style="text-align: center;">Học lap trinh Laravel tại Unicode</h1>
-<?php 
-if (env('APP_ENV') == 'production'){
-    //call api live
-    echo 'call api live';
-}else{
-    //call api sandbox
-    echo 'call api sandbox';
-}
+<a href="<?php echo route('admin.tintuc', ['id'=>1, 'slug'=>'tin-tuc-the-gioi']); ?>">Xem tin tuc</a>
+
+24:25
