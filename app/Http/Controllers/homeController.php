8e120528c2ac9341cf62845d2d8085137ab53d3d
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-02-06 12:42:56 +0700

    add bai8

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -4,11 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\View;
+
 class homeController extends Controller
 {
     //Action index()
     public function index(){
-        return 'Home';
+        $title = "Hoc lap trinh web tai unicode";
+        $content = 'Hoc lap trinh laravel 8.x tai unicode';
+
+        // 'title'=>$title,
+        // 'content' =>$content,
+        
+        //compact('title', 'content')
+
+        return view('home')->with(['title'=>$title, 'content'=>$content]); //load view home.php
+        //return View::make('home', compact('title', 'content'));
+        //return View::make('home')->with(['title'=>$title, 'content'=>$content]);
+        // $contentView = view('home')->render();
+        //$contentView = $contentView->render();
+        // dd($contentView);
+        //return $contentView;
+    
     }
 
     //Action getNews()
@@ -18,5 +35,9 @@ class homeController extends Controller
 
     public function getCategories(){
         return 'Chuyen muc';
+    }
+
+    public function getProductDetail($id){
+        return view('clients.products.detail', compact('id'));
     }
 }
