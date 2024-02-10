ae810a7521340009828f861be079f9129dfd342d
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-02-10 21:16:13 +0700

    add bai13

---
+++ resources/views/clients/products.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.client');
+@section('title')
+    {{$title}}
+@endsection
+
+@section('sidebar')
+    {{-- @parent --}}
+    <h3>Products sidebar</h3>
+@endsection
+
+@section('content')
+    <h1>SAN PHAM</h1>
+@endsection
+
+@section('css')
+    <style>
+        header{
+        background: yellow;
+        color: #333;
+        }
+    </style>
+@endsection
+
+@section('js')
+
+@endsection
