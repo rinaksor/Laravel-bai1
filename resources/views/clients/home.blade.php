0dfa2479eef9bf0808e563d22332152fc9573c9e
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-02-11 11:58:41 +0700

    add bai14

--- resources/views/clients/home.blade.php
+++ resources/views/clients/home.blade.php
@@ -10,30 +10,14 @@
 
 @section('content')
     <h1>Trang chu</h1>
-    <button type="button" class="show">Show</button>
+    @include('clients.contents.slide')
+    @include('clients.contents.about')
 @endsection
 
 @section('css')
-    {{-- header{
-        background: blue;
-        color: #fff;
-    } --}}
 
-    <style>
-        header{
-        background: blue;
-        color: #fff;
-        }
-    </style>
 @endsection
 
 @section('js')
-    {{-- document.querySelector('.show').onclick = function(){
-        alert('Thanh cong');
-    } --}}
-    <script>
-        document.querySelector('.show').onclick = function(){
-        alert('Thanh cong');
-        }
-    </script>
+
 @endsection
--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -5,29 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('content') - Unicode</title>
-    {{-- <style type="text/css"> --}}
-        @ield('css')
-    {{-- </style> --}}
+    <link rel="stylesheet" href="{{asset('assets/clients/css/boostrap.min.css.map')}}">
+    <link rel="stylesheet" href="{{asset('assets/clients/css/boostrap.styles.css')}}">
+    @ield('css')
 </head>
 <body>
-    <header>
-        <h1>HEADER</h1>
-    </header>
-    <main>
-        <aside>
-            @section('sidebar')
-                @include('clients.blocks.sidebar')
-            @show
-        </aside>
-        <div class="content">
-            @yield('content');
+    @include('clients.blocks.header')
+    <main class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <aside>
+                        @section('sidebar')
+                            @include('clients.blocks.sidebar')
+                        @show
+                    </aside>
+                </div>
+            </div>
+            <div class="col-8">
+                <div class="content">
+                    @yield('content');
+                </div>
+            </div>
         </div>
     </main>
-    <footer>
-        <h1>FOOTER</h1>
-    </footer>
-    {{-- <script type="text/javascrip"> --}}
-        @yield('js')
-    {{-- </script> --}}
+    @include('clients.blocks.footer')
+    <script src="{{asset('assets/clients/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/clients/js.custom.js')}}"></script>
+    @yield('js')
 </body>
 </html>
