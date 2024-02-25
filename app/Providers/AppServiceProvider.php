2dce6389cadf26a6cf78e563b321231604e45e7d
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-02-26 00:19:32 +0700

    sửa middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,10 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
         Blade::if('env', function($value){ //@env('local)
             //Tra ve gia tri boolen
-            if(config('app.env')== $value){
+            if(config('app.env') === $value){
                 return true;
             }
             return false;
