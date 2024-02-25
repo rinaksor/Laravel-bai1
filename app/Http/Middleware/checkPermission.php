4db739c5d16b861d4ba5534b5c1198c6241515ee
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-02-25 22:11:26 +0700

    sửa lỗi middleware

--- app/Http/Middleware/checkPermission.php
+++ app/Http/Middleware/checkPermission.php
@@ -13,7 +13,7 @@ class checkPermission
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
         //$homeUrl = route('home');
         //return redirect($homeUrl);
