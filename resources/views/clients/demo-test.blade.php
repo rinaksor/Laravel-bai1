b3c753f835c28ae78d37f65861fdd96fe628ae2b
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-02-26 11:30:57 +0700

    add bai 18

---
+++ resources/views/clients/demo-test.blade.php
@@ -0,0 +1,2 @@
+<h2>Demo view Unicode</h2>
+<h3>{{$title}}</h3>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 use App\Http\Controllers\HomeController;
-use Termwind\Components\Hr;
+
+use Illuminate\Http\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,43 @@ Route::get('/them-san-pham', [HomeController::class, 'getAdd']);
 //Route::post('/them-san-pham', [HomeController::class, 'postAdd']);
 
 Route::put('/them-san-pham', [HomeController::class, 'putAdd']);
+
+Route::get('lay-thong-tin',[HomeController::class, 'getArr']);
+
+Route::get('demo-response', function(){
+
+//$content = '<h2>Học lập trình tại Unicode</h2>';
+// $content = 'Học lập trình tại Unicode';
+//    $content = json_encode([
+//     'Item 1',
+//     'Item 2',
+//     'Item 3'
+
+//    ]);
+//     $response = (new Response($content))->header('Content-Type', 'application/json');
+//    dd($response);
+
+// $response = response();
+// dd($response);
+
+//$response = new Response('', 201);
+//$response = response('Học lập trình tại Unicode', 404);
+
+// $response = (new Response())->cookie('unicode', 'Training PHP - Laravel', 30);
+// return $response;
+
+//return view('clients.demo-test');
+
+    $response = response()
+    ->view('clients.demo-test', [
+        'title' => 'Hoc HTTP Response'
+    ], 201)
+    ->header('Content-Type', 'application/json')
+    ->header('API-Key', '123456');
+
+    return $response;
+});
+
+// Route::get('demo-response-2', function(Request $request){
+//     return $request->cookie('unicode');
+// });
