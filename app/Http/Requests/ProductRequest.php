50cac68f6423b6969ba38a9786a112f24d9e3d3d
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-03-12 00:34:15 +0700

    add bai27

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -16,7 +16,7 @@ class ProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- resources/views/clients/add.blade.php
+++ resources/views/clients/add.blade.php
@@ -5,24 +5,24 @@
 
 @section('content')
     <h1>Them san pham</h1>
-    <form action="" method="POST">
-        @error('record')
-            <div class="alert alert-danger text-center">{{$message}}</div>
-        @enderror
+    <form action="{{route('post-add')}}" method="POST" id="product-form">
+
+            <div class="alert alert-danger text-center msg" style="display: none;"></div>
+
         <div class="mb-3">
             <label for="">Ten san pham</label>
             <input type="text" class="form-control" name="product_name" placeholder="Ten san pham ..." value="{{old('product_name')}}"/>
-            @error('product_name')
-                <span style="color: red">{{$message}}</span>
-            @enderror
+            
+                <span style="color: red" class="error product_name_error"></span>
+            
         </div>
 
         <div class="mb-3">
             <label for="">Gia san pham</label>
             <input type="text" class="form-control" name="product_price" placeholder="Gia san pham ..." value="{{old('product_price')}}" />
-            @error('product_price')
-                <span style="color: red">{{$message}}</span>
-            @enderror
+           
+                <span style="color: red"  class="error product_price_error"></span>
+          
         </div>
         <button type="submit" class="btn btn-primary">Them moi</button>
 
@@ -35,5 +35,56 @@
 @endsection
 
 @section('js')
+<script>
+    $(document).ready(function(){
+        $('#product-form').on('submit', function(e){
+            e.preventDefault();
+            
+            let productName = $('input[name="product_name"]').val().trim();
 
+            let productPrice = $('input[name="product_price"]').val().trim();
+            
+            let actionUrl = $(this).attr('action');
+
+            let csrfToken = $(this).find('input[name="_token"]').val();
+
+            //console.log(csrfToken);
+
+            $('.error').text('');
+
+            $.ajax(
+                {
+                    url: actionUrl,
+                    type: 'POST',
+                    data: {
+                        product_name: productName,
+                        product_price: productPrice,
+                        _token: csrfToken
+
+                    },
+                    dataType: 'json',
+                    success: function(response){
+                        console.log(response);
+                    },
+                    error: function(error){
+                    
+                            $('.msg').show();
+
+                            let responseJSON = error.responseJSON.errors;
+
+                            if (Object.keys(responseJSON).length>0){
+
+                                $('.msg').text( responseJSON,msg);
+                                
+                                for (let key in responseJSON){
+
+                                    $('.'+key+'_error').text(responseJSON[key][0]);
+                            }
+                        }
+                    }
+                }
+            )
+        });
+    });
+</script>
 @endsection
